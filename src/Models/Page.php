913ea0cada2f88f08aa42388b5ae93e3cabbf9e8
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-01-14 16:18:06 +0000

    Code refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -31,9 +31,6 @@ class Page extends Model
      */
     protected $primaryKey = 'page_id';
 
-    /**
-     * @var array The model table column to convert to dates on insert/update
-     */
     public $timestamps = true;
 
     protected $guarded = [];
